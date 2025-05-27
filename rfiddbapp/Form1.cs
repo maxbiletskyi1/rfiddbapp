35662df8186629c2f99aa9dc776584e752ff1de5
Max Biletskyi <biletskyimaxacc@gmail.com>
2025-05-27 14:10:41 +0200

    Commit-4

--- rfiddbapp/Form1.cs
+++ rfiddbapp/Form1.cs
@@ -11,38 +11,52 @@ using Org.BouncyCastle.Bcpg;
 using System.Text;
 using MySqlX.XDevAPI.Common;
 using System.Runtime.InteropServices;
-using System.Threading;
+using System.Threading.Tasks;
+using rfiddbapp;
+using Org.BouncyCastle.Asn1.Cmp;
 namespace rfiddbapp
 {
     //Lecture de la BD
     public partial class Form1 : Form
     {
         static MySqlConnection db = new MySqlConnection("server=localhost;user=root;database=vigichantier;port=3308;password=Makson2004belka!");
-        static int scannedId;
+        static string scannedId;
         static string scannedMarqueT = "";
 
-        static int id_Badge;
+        static string id_Badge;
         static string MarqueT = "";
 
         public Form1()
         {
             InitializeComponent();
             SocketReader.Socket();
-            SocketReader.ReadData();
+            readAsyncData();
         }
 
-        void readDB(string name, int id)
+        private async void readAsyncData()
         {
-            string query = "SELECT * FROM Badge;";
-            if (idInput.Text != "" && nameInput.Text != "")
+            //btnStart.Enabled = false; // Disable button to prevent multiple clicks
+            await Task.Run(() =>
             {
-                scannedId = Int32.Parse(idInput.Text);
+                SocketReader.ReadData();
+            });
+            //btnStart.Enabled = true;  // Re-enable button after task finishes+
+        }
+
+
+        void readDB(string name, string id)
+        {
+            
+            string query = "SELECT * FROM Badge;";
+            /*if (idInput.Text != "" && nameInput.Text != "")
+            {
+                scannedId = idInput.Text;
                 scannedMarqueT = nameInput.Text;
             }
             else
             {
                 MessageBox.Show("Le champ ne peut pas etre vide!");
-            }
+            }*/
             if (db.State == ConnectionState.Open)
             {
                 using (MySqlCommand cmd = new MySqlCommand(query, db))
@@ -53,18 +67,22 @@ namespace rfiddbapp
                         {
                             while (reader.Read()) // Loop through results
                             {
-                                id_Badge = reader.GetInt32("id_Badge");
+                                id_Badge = reader.GetString("id_Badge");
                                 MarqueT = reader.GetString("MarqueT");
                                 Console.WriteLine($"ID: {id_Badge}, Marque Tag: {MarqueT}");
-                                if (id_Badge == scannedId && MarqueT == scannedMarqueT)
+                                //if (id_Badge == scannedId && MarqueT == scannedMarqueT)
+                                if (id_Badge == SocketReader.toOutput)
                                 {
                                     messageLbl.Text = "Access granted";
+                                    MessageBox.Show(SocketReader.toOutput);
                                     break;
                                 }
                             }
-                            if (id_Badge != scannedId || MarqueT != scannedMarqueT)
+                            //if (id_Badge != scannedId || MarqueT != scannedMarqueT)
+                            if (id_Badge != SocketReader.toOutput)
                             {
                                 messageLbl.Text = "Access denied";
+                                MessageBox.Show(SocketReader.toOutput);
                             }
                         }
                     }
@@ -94,146 +112,101 @@ namespace rfiddbapp
         }
     }
 }
-/*
-public class PortChat
-{
-    static bool _continue;
-    static SerialPort _serialPort;
-
-    public static void SerialReader()
-    {
-        string name;
-        string message;
-        StringComparer stringComparer = StringComparer.OrdinalIgnoreCase;
-        //Thread readThread = new Thread(Read);
-
-        // Create a new SerialPort object with default settings.
-        _serialPort = new SerialPort("COM5", 9600, Parity.None, 8, StopBits.One);
-
-        // Set the read/write timeouts
-        _serialPort.ReadTimeout = 500;
-        _serialPort.WriteTimeout = 500;
-
-        _serialPort.Open();
-        _continue = true;
-        //readThread.Start();
-
-        while (_continue)
-        {
-            if (stringComparer.Equals("quit", message))
-            {
-                _continue = false;
-            }
-            else
-            {
-                _serialPort.WriteLine(
-                    String.Format("<{0}>: {1}", name, message));
-            }
-        }
-
-        //readThread.Join();
-    }
-
-    public static void Read()
-    {
-        while (_continue)
-        {
-            try
-            {
-                string message = _serialPort.ReadLine();
-                MessageBox.Show(message);
-            }
-            catch (TimeoutException) { }
-        }
-    }
-}
-*/
 
 //Lecture de tag RFID
 public class SocketReader
+{
+    //static Thread TlectureRFID = new Thread(ReadData);
+    //static TcpClient client = new TcpClient();
+    static IPAddress address = IPAddress.Parse("192.168.30.10");
+    static IPEndPoint endpoint = new IPEndPoint(address, 10001);
+    static Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+    static byte[] responseBytes = new byte[256];
+    static char[] responseChars = new char[256];
+    public static string result = "";
+    public static string toOutput = "";
+
+    public static void Socket()
     {
-        //static Thread TlectureRFID = new Thread(ReadData);
-        //static TcpClient client = new TcpClient();
-        static IPAddress address = IPAddress.Parse("192.168.30.10");
-        static IPEndPoint endpoint = new IPEndPoint(address, 10001);
-        static Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
-        static byte[] responseBytes = new byte[256];
-        static char[] responseChars = new char[256];
-        static string result = "";
-        static string toOutput = "";
-        public static void Socket()
+        try
         {
-            try
-            {
-                //client.Connect(endpoint);
-                socket.Connect(endpoint);
-                MessageBox.Show("Connected to socket!");
-                // Add code to read from the socket
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show($"Error connecting to socket: {ex.Message}");
-            }
+            //client.Connect(endpoint);
+            socket.Connect(endpoint);
+            MessageBox.Show("Connected to socket!");
+            // Add code to read from the socket
         }
-
-// https://learn.microsoft.com/en-us/dotnet/api/system.net.sockets.socket?view=net-9.0
-
-    public static void ReadData() 
+        catch (Exception ex)
         {
-            try
+            MessageBox.Show($"Error connecting to socket: {ex.Message}");
+        }
+    }
+
+    // https://learn.microsoft.com/en-us/dotnet/api/system.net.sockets.socket?view=net-9.0
+
+    public static void ReadData()
+    {
+        try
+        {
+            while (true)
             {
-                while (true)
-                {
-                    //socket.Blocking = false;
-                    Thread.Sleep(2000);
-                    int bytesReceived = socket.Receive(responseBytes);
+                //socket.Blocking = false;
+                Thread.Sleep(2000);
+                int bytesReceived = socket.Receive(responseBytes);
 
-                    // Receiving 0 bytes means EOF has been reached
-                    if (bytesReceived == 0) break;
+                // Receiving 0 bytes means EOF has been reached
+                if (bytesReceived == 0) break;
 
-                    // Convert byteCount bytes to ASCII characters using the 'responseChars' buffer as destination
-                    int charCount = Encoding.ASCII.GetChars(responseBytes, 0, bytesReceived, responseChars, 0);
+                // Convert byteCount bytes to ASCII characters using the 'responseChars' buffer as destination
+                int charCount = Encoding.ASCII.GetChars(responseBytes, 0, bytesReceived, responseChars, 0);
 
-                    // Print the contents of the 'responseChars' buffer to Console.Out
-                    result = new string(responseChars);
+                // Print the contents of the 'responseChars' buffer to Console.Out
+                result = new string(responseChars);
 
-                    //MessageBox.Show(result[3..9]);
-                    MessageBox.Show(ParseId(result));
+                //MessageBox.Show(result[3..9]);
+                MessageBox.Show(ParseId(result));
+
+                toOutput = ParseId(result);
 
                 //socket.Blocking = true;
 
 
                 //break;
             }
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show($"Error: {ex.Message}");
-            }
         }
-    //[A60611FD01] or [60611FD01]
-    public static string ParseId(string result) 
+        catch (Exception ex)
         {
-        toOutput = "";
-            if (result[0]=='[')
-            {
-                int count = 1;
-                while (result[count] != ']')
-                {
-                    toOutput += result[count];
-                    count++;
-                }
-            }
-            if (toOutput.Length == 10)
-            {
-                return toOutput[2..8];
-            }
-            else if (toOutput.Length == 9)
-            { 
-                return toOutput[1..7];
-        }
-        return toOutput;
+            MessageBox.Show($"Error: {ex.Message}");
         }
     }
+    //[A60611FD01] or [60611FD01]
+    public static string ParseId(string result)
+    {
+        toOutput = "";
+        if (result[0] == '[')
+        {
+            int count = 1;
+            while (result[count] != ']')
+            {
+                toOutput += result[count];
+                count++;
+            }
+        }
+        if (toOutput.Length == 10)
+        {
+            return toOutput[2..8];
+        }
+        else if (toOutput.Length == 9)
+        {
+            return toOutput[1..7];
+        }
+        return toOutput;
+    }
+
+    public static void CloseSocket()
+    {
+        socket.Shutdown(SocketShutdown.Both);
+        socket.Close();
+    }
+}
 
    
